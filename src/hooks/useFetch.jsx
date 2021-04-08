3b7851d31f689d90324f91c056b3f3e7c2815ff4
Daniel Taylor English <danieltaylorenglish@gmail.com>
2021-04-08 15:00:36 -0700

    useFetch code cleanup

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -4,22 +4,18 @@ import API from '../api';
 const useFetch = (query) => {
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
-  // const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
-      // setIsLoading(true);
       try {
-        const { data } = await API.get(`/weather?q=${query}`);
-        setData(data);
-        // setIsLoading(false);
+        const response = await API.get(`/weather?q=${query}`);
+        setData(response.data);
       } catch (err) {
         setError(err);
       }
     };
     fetchData();
   }, []);
-  // return { data, error, isLoading };
   return { data, error };
 }
 
